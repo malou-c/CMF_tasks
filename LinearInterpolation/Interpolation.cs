ac8708f973dae5460bac77135743e3081a3ea52b
malou-c <ksh1957nr@mail.ru>
2021-09-25 18:27:41 +0700

    Добавьте файлы проекта.

---
+++ LinearInterpolation/Interpolation.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinearInterpolation
+{
+    class Interpolation
+    {
+        // Function linear interpolation  (( X — X0 ) * ( Y1 — Y0) / ( X1 — X0) ) + Y0
+        private static double CalculateLinearInterpolation(double[] xy0, double[] xy1, double X)
+            => ((X - xy0[0] ) * (xy1[1] - xy0[1]) / (xy1[0] - xy0[0])) + xy0[1];
+        public static double LinearZ(double[][] xyArray, double z)
+        {
+            int GetBottomIdx()
+            {
+                int idxBottom = -1;
+                var idx = 1;
+                while (idx < xyArray.Length)
+                {
+                    if (xyArray[idx][0] > z)
+                    {
+                        idxBottom = idx - 1;
+                        break;
+                    }
+                    idx++;
+                }
+                return idxBottom;
+            }
+            // check z in (min(x) ; max(x)) 
+            if (z < xyArray[0][0] || z > xyArray[xyArray.Length - 1][0])
+                return double.NaN;
+
+            int idx0 = GetBottomIdx();
+            int idx1 = idx0 + 1;
+            return CalculateLinearInterpolation(xyArray[idx0], xyArray[idx1], z);
+        }
+    }
+}
---
+++ LinearInterpolation/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LinearInterpolation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<double, double> func = x => x * x + 3;
+            Func<double, double[]> row = x => new double[2] { x, func(x) };
+            double[] xs = { 1, 2, 3, 5, 6, 12};
+            var xyArray = xs.Select(row).ToArray();
+
+            foreach (double[] el in xyArray)
+                Console.WriteLine($"x = {el[0]}  y = {el[1]}");
+
+            // test
+            Console.WriteLine($"Interpolation f(z):");
+            double z = 14.23578;
+            Console.WriteLine($"f({z}) = {Interpolation.LinearZ(xyArray, z)}");
+
+        }
+    }
+}
